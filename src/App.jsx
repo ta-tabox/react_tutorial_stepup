971e7504ca12c4b1cd4d29c7100af11aa8cc73df
Daiki Tanaka <67009309+tanktabox@users.noreply.github.com>
2022-07-01 23:07:48 +0900

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import { BrowserRouter, Link } from "react-router-dom";
+
+import { Home } from "./Home";
+import { Page1 } from "./Page1";
+import { Page2 } from "./Page2";
 import "./styles.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <Link to="/">Home</Link>
+      <br/>
+      <Link to="/page1">Page1</Link>
+      <br/>
+      <Link to="/page2">Page2</Link>
+      <Home />
+      <Page1 />
+      <Page2 />
+    </div>
+    </BrowserRouter>
+  );
 }
